7c632e423172e168119f0eb8fb867d9a361032ca
JorgeRodriguez <jorge.rodriguez1@udea.edu.co>
2024-05-06 18:42:35 -0500

    Selección de variables y desempeño de modelos

--- A_Funciones.py
+++ A_Funciones.py
@@ -57,3 +57,27 @@ def imputar_f (df,list_cat):
 
     df =pd.concat([df_n,df_c],axis=1)
     return df
+
+def sel_variables(modelos, X, y, threshold):
+    
+    var_names_ac = np.array([])
+    for modelo in modelos:
+        #modelo=modelos[i]
+        modelo.fit(X,y)
+        sel = SelectFromModel(modelo, prefit = True, threshold = threshold)
+        var_names = modelo.feature_names_in_[sel.get_support()]
+        var_names_ac = np.append(var_names_ac, var_names)
+        var_names_ac = np.unique(var_names_ac)
+    
+    return var_names_ac
+
+def medir_modelos(modelos, scoring, X, y, cv):
+
+    metric_modelos = pd.DataFrame()
+    for modelo in modelos:
+        scores = cross_val_score(modelo, X, y, scoring = scoring, cv = cv )
+        pdscores = pd.DataFrame(scores)
+        metric_modelos = pd.concat([metric_modelos,pdscores], axis = 1)
+    
+    metric_modelos.columns = ["Random_Forest","DecissionTree","XGB_Regressor"]
+    return metric_modelos
